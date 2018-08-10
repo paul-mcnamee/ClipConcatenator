2f7c72267a15d8ac6f3b84cef5958680567be48e
Paul McNamee <paulmc121@gmail.com>
2018-08-09 23:07:38 -0600

    added some stuff to the gitignore

--- CleanUpClips.py
+++ CleanUpClips.py
@@ -1,5 +1,6 @@
 import configparser
 import shutil
+import time
 
 
 config = configparser.ConfigParser()
@@ -8,4 +9,6 @@ directory_to_remove = config.get('paths', 'output_dir')
 keep_clips = config.getboolean('settings', 'keep_clips')
 
 if not keep_clips:
+    # wait 15 minutes to ensure the last clip was uploaded...
+    time.sleep(900)
     shutil.rmtree(directory_to_remove, ignore_errors=True)
